7e689b818422dccac4fa4815508e70d4eede2c26
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-10-05 17:48:37 +0100

    update

--- src/Image.jsx
+++ src/Image.jsx
@@ -17,7 +17,7 @@ export const Image = ({ artist }) => {
       />
       <div className="text-[1rem] absolute bottom-0 right-0 py-2 px-4 gap-2 rounded-tl-lg bg-white border-t border-r  z-20  font-mono flex ">
         {/* <img src="/assets/Star.svg" /> */}
-        🌟 16
+        🌟16
       </div>
     </div>
   );
