6f4f8a978bca98487498441280ab5dbb837e0373
tohir babatunde <tohirbabs@gmail.com>
2024-02-21 09:41:23 +0100

    updates

--- src/Image.jsx
+++ src/Image.jsx
@@ -4,7 +4,7 @@ export const Image = ({ artist }) => {
     <div className="overflow-hidden flex rounded-xl my-2 lg:my-4 items-center justify-center relative">
       <a
         href={`https://twitter.com/${artist}`}
-        className="absolute top-3 z-20 left-4 text-sm lg:text-base font-mono text-white mix-blend-exclusion underline"
+        className="absolute lg:top-3 z-20 lg:left-4 top-2 left-2   text-xs lg:text-base font-mono text-white mix-blend-exclusion underline"
       >
         @{artist}
       </a>
